50090a47badd92ecc4407120fdbe62384729ab57
Scarrange <xMizor@gmail.com>
2024-04-19 09:53:21 +0200

    DTO Klassen

---
+++ src/film/rest/schauspielerDTO.entity.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+import { IsISO8601, IsOptional, Matches, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+/**
+ * Entity-Klasse für Schauspieler ohne TypeORM.
+ */
+export class SchauspielerDTO {
+    @Matches('^\\w.*')
+    @MaxLength(40)
+    @ApiProperty({ example: 'Der Name', type: String })
+    readonly name!: string;
+
+    @IsOptional()
+    @IsISO8601({ strict: true })
+    @ApiProperty({ example: '2023-12-31', type: Date })
+    readonly geburtsdatum: Date | undefined;
+}
+/* eslint-enable @typescript-eslint/no-magic-numbers */
