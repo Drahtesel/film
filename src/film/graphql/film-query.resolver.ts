c44107e3b97c34c5259f9e7d1cf2dd0bda3432ce
Robin Kraus <krro1024@h-ka.de>
2024-04-20 13:11:00 +0200

    graphql

---
+++ src/film/graphql/film-query.resolver.ts
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2021 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import { Args, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { UseFilters, UseInterceptors } from '@nestjs/common';
+import { Film } from '../entity/film.entity.js';
+import { FilmReadService } from '../service/film-read.service.js';
+import { HttpExceptionFilter } from './http-exception.filter.js';
+import { Public } from 'nest-keycloak-connect';
+import { ResponseTimeInterceptor } from '../../logger/response-time.interceptor.js';
+import { type Suchkriterien } from '../service/suchkriterien.js';
+import { getLogger } from '../../logger/logger.js';
+
+export interface IdInput {
+    readonly id: number;
+}
+
+export interface SuchkriterienInput {
+    readonly suchkriterien: Suchkriterien;
+}
+
+@Resolver((_: any) => Film)
+@UseFilters(HttpExceptionFilter)
+@UseInterceptors(ResponseTimeInterceptor)
+export class FilmQueryResolver {
+    readonly #service: FilmReadService;
+
+    readonly #logger = getLogger(FilmQueryResolver.name);
+
+    constructor(service: FilmReadService) {
+        this.#service = service;
+    }
+
+    @Query('film')
+    @Public()
+    async findById(@Args() { id }: IdInput) {
+        this.#logger.debug('findById: id=%d', id);
+
+        const film = await this.#service.findById({ id });
+
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug(
+                'findById: film=%s, distributor=%o',
+                film.toString(),
+                film.distributor,
+            );
+        }
+        return film;
+    }
+
+    @Query('filme')
+    @Public()
+    async find(@Args() input: SuchkriterienInput | undefined) {
+        this.#logger.debug('find: input=%o', input);
+        const filme = await this.#service.find(input?.suchkriterien);
+        this.#logger.debug('find: buecher=%o', filme);
+        return filme;
+    }
+
+    @ResolveField('rabatt')
+    rabatt(@Parent() film: Film, short: boolean | undefined) {
+        if (this.#logger.isLevelEnabled('debug')) {
+            this.#logger.debug(
+                'rabatt: film=%s, short=%s',
+                film.toString(),
+                short,
+            );
+        }
+        const rabatt = film.rabatt ?? 0;
+        const shortStr = short === undefined || short ? '%' : 'Prozent';
+        // eslint-disable-next-line @typescript-eslint/no-magic-numbers
+        return `${(rabatt * 100).toFixed(2)} ${shortStr}`;
+    }
+}
