49a87fe777dbdfebcc5f3f0eb94bf36975345661
Robin Kraus <krro1024@h-ka.de>
2024-04-27 17:13:03 +0200

    Tests

---
+++ __tests__/film/film-GET.controller.test.ts
@@ -0,0 +1,194 @@
+/* eslint-disable no-underscore-dangle */
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { Distributor } from '../../src/film/entity/distributor.entity';
+import { type ErrorResponse } from './error-response.js';
+import { type FilmeModel } from '../../src/film/rest/film-get.controller.js';
+import { HttpStatus } from '@nestjs/common';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const distributorVorhanden = 'a';
+const distributorNichtVorhanden = 'xx';
+const schlagwortVorhanden = 'action';
+const schlagwortNichtVorhanden = 'drama';
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('GET /rest', () => {
+    let baseURL: string;
+    let client: AxiosInstance;
+
+    beforeAll(async () => {
+        await startServer();
+        baseURL = `https://${host}:${port}/rest`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: () => true,
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Alle Filme', async () => {
+        // given
+
+        // when
+        const { status, headers, data }: AxiosResponse<FilmeModel> =
+            await client.get('/');
+
+        // then
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu); // eslint-disable-line sonarjs/no-duplicate-string
+        expect(data).toBeDefined();
+
+        const { filme } = data._embedded;
+
+        filme
+            .map((film) => film._links.self.href)
+            .forEach((selfLink) => {
+                // eslint-disable-next-line security/detect-non-literal-regexp, security-node/non-literal-reg-expr
+                expect(selfLink).toMatch(new RegExp(`^${baseURL}`, 'iu'));
+            });
+    });
+
+    test('Filme mit einem Teil-Distributorname suchen', async () => {
+        // given
+        const params = { distributor: distributorVorhanden };
+
+        // when
+        const { status, headers, data }: AxiosResponse<FilmeModel> =
+            await client.get('/', { params });
+
+        // then
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        expect(data).toBeDefined();
+
+        const { filme } = data._embedded;
+
+        // Jedes Filme hat einen Distributor mit dem Teilstring 'a'
+        filme
+            .map((film) => film.distributor)
+            .forEach((distributor) =>
+                expect(distributor.name.toLowerCase()).toEqual(
+                    expect.stringContaining(distributorVorhanden),
+                ),
+            );
+    });
+
+    test('Filme zu einem nicht vorhandenen Teil-Distributorname suchen', async () => {
+        // given
+        const params = { distributor: distributorNichtVorhanden };
+
+        // when
+        const { status, data }: AxiosResponse<ErrorResponse> = await client.get(
+            '/',
+            { params },
+        );
+
+        // then
+        expect(status).toBe(HttpStatus.NOT_FOUND);
+
+        const { error, statusCode } = data;
+
+        expect(error).toBe('Not Found');
+        expect(statusCode).toBe(HttpStatus.NOT_FOUND);
+    });
+
+    test('Mind. 1 Film mit vorhandenem Schlagwort', async () => {
+        // given
+        const params = { [schlagwortVorhanden]: 'true' };
+
+        // when
+        const { status, headers, data }: AxiosResponse<FilmeModel> =
+            await client.get('/', { params });
+
+        // then
+        expect(status).toBe(HttpStatus.OK);
+        expect(headers['content-type']).toMatch(/json/iu);
+        // JSON-Array mit mind. 1 JSON-Objekt
+        expect(data).toBeDefined();
+
+        const { filme } = data._embedded;
+
+        // Jedes Buch hat im Array der Schlagwoerter z.B. "javascript"
+        filme
+            .map((film) => film.schlagwoerter)
+            .forEach((schlagwoerter) =>
+                expect(schlagwoerter).toEqual(
+                    expect.arrayContaining([schlagwortVorhanden.toUpperCase()]),
+                ),
+            );
+    });
+
+    test('Keine Filme zu einem nicht vorhandenen Schlagwort', async () => {
+        // given
+        const params = { [schlagwortNichtVorhanden]: 'true' };
+
+        // when
+        const { status, data }: AxiosResponse<ErrorResponse> = await client.get(
+            '/',
+            { params },
+        );
+
+        // then
+        expect(status).toBe(HttpStatus.NOT_FOUND);
+
+        const { error, statusCode } = data;
+
+        expect(error).toBe('Not Found');
+        expect(statusCode).toBe(HttpStatus.NOT_FOUND);
+    });
+
+    test('Keine Filme zu einer nicht-vorhandenen Property', async () => {
+        // given
+        const params = { foo: 'bar' };
+
+        // when
+        const { status, data }: AxiosResponse<ErrorResponse> = await client.get(
+            '/',
+            { params },
+        );
+
+        // then
+        expect(status).toBe(HttpStatus.NOT_FOUND);
+
+        const { error, statusCode } = data;
+
+        expect(error).toBe('Not Found');
+        expect(statusCode).toBe(HttpStatus.NOT_FOUND);
+    });
+});
+/* eslint-enable no-underscore-dangle */
---
+++ __tests__/film/film-POST.controller.test.ts
@@ -0,0 +1,237 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import axios, { type AxiosInstance, type AxiosResponse } from 'axios';
+import {
+    host,
+    httpsAgent,
+    port,
+    shutdownServer,
+    startServer,
+} from '../testserver.js';
+import { type ErrorResponse } from './error-response.js';
+import { type FilmDTO } from '../../src/film/rest/filmDTO.entity.js';
+import { FilmReadService } from '../../src/film/service/film-read.service.js';
+import { HttpStatus } from '@nestjs/common';
+import { loginRest } from '../login.js';
+import { todo } from 'node:test';
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const neuerFilm: FilmDTO = {
+    titel: 'Revenant',
+    laenge: 120,
+    rating: 1,
+    filmart: 'KINOFASSUNG',
+    preis: 99.99,
+    rabatt: 0.123,
+    streambar: true,
+    erscheinungsdatum: new Date('2022-02-28'),
+    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
+    distributor: {
+        name: 'Universal',
+        umsatz: 0,
+        homepage: 'https://Universal/',
+    },
+    schauspielerListe: [
+        {
+            name: 'Smith',
+            geburtsdatum: new Date('1999-12-31'),
+        },
+    ],
+};
+const neuerFilmInvalid: Record<string, unknown> = {
+    titel: 'Star Wars',
+    laenge: 120,
+    rating: -1,
+    filmart: 'ORIGINAL',
+    preis: -1,
+    rabatt: 2,
+    streambar: true,
+    erscheinungsdatum: '12345-123-123',
+    distributor: {
+        name: '?!',
+        umsatz: -1,
+        homepage: 'anyHomepage',
+    },
+};
+// Name + Erscheinungsdatum müssen eindeutig sein
+const neuesBuchIsbnExistiert: FilmDTO = {
+    titel: '978-3-897-22583-1',
+    laenge: 120,
+    rating: 1,
+    filmart: 'KINOFASSUNG',
+    preis: 99.99,
+    rabatt: 0.099,
+    streambar: true,
+    erscheinungsdatum: new Date('2022-02-28'),
+    schlagwoerter: ['ACTION', 'DRAMA'],
+    distributor: {
+        name: 'Titelpostisbn',
+        umsatz: 0,
+        homepage: 'https://post.isbn/',
+    },
+    schauspielerListe: undefined,
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+// Test-Suite
+// eslint-disable-next-line max-lines-per-function
+describe('POST /rest', () => {
+    let client: AxiosInstance;
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json', // eslint-disable-line @typescript-eslint/naming-convention
+    };
+
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        await startServer();
+        const baseURL = `https://${host}:${port}`;
+        client = axios.create({
+            baseURL,
+            httpsAgent,
+            validateStatus: (status) => status < 500, // eslint-disable-line @typescript-eslint/no-magic-numbers
+        });
+    });
+
+    afterAll(async () => {
+        await shutdownServer();
+    });
+
+    test('Neues Buch', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<string> = await client.post(
+            '/rest',
+            neuerFilm,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.CREATED);
+
+        const { location } = response.headers as { location: string };
+
+        expect(location).toBeDefined();
+
+        // ID nach dem letzten "/"
+        const indexLastSlash: number = location.lastIndexOf('/');
+
+        expect(indexLastSlash).not.toBe(-1);
+
+        const idStr = location.slice(indexLastSlash + 1);
+
+        expect(idStr).toBeDefined();
+        expect(FilmReadService.ID_PATTERN.test(idStr)).toBe(true);
+
+        expect(data).toBe('');
+    });
+
+    test('Neues Buch mit ungueltigen Daten', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+        const expectedMsg = [
+            expect.stringMatching(/^isbn /u),
+            expect.stringMatching(/^rating /u),
+            expect.stringMatching(/^art /u),
+            expect.stringMatching(/^preis /u),
+            expect.stringMatching(/^rabatt /u),
+            expect.stringMatching(/^datum /u),
+            expect.stringMatching(/^homepage /u),
+            expect.stringMatching(/^titel.titel /u),
+        ];
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerFilmInvalid,
+            { headers },
+        );
+
+        // then
+        const { status, data } = response;
+
+        expect(status).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const messages: string[] = data.message;
+
+        expect(messages).toBeDefined();
+        expect(messages).toHaveLength(expectedMsg.length);
+        expect(messages).toEqual(expect.arrayContaining(expectedMsg));
+    });
+
+    test('Neues Buch, aber die ISBN existiert bereits', async () => {
+        // given
+        const token = await loginRest(client);
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<ErrorResponse> = await client.post(
+            '/rest',
+            neuesBuchIsbnExistiert,
+            { headers },
+        );
+
+        // then
+        const { data } = response;
+
+        const { message, statusCode } = data;
+
+        expect(message).toEqual(expect.stringContaining('ISBN'));
+        expect(statusCode).toBe(HttpStatus.UNPROCESSABLE_ENTITY);
+    });
+
+    test('Neues Buch, aber ohne Token', async () => {
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerFilm,
+        );
+
+        // then
+        expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
+    });
+
+    test('Neues Buch, aber mit falschem Token', async () => {
+        // given
+        const token = 'FALSCH';
+        headers.Authorization = `Bearer ${token}`;
+
+        // when
+        const response: AxiosResponse<Record<string, any>> = await client.post(
+            '/rest',
+            neuerFilm,
+            { headers },
+        );
+
+        // then
+        expect(response.status).toBe(HttpStatus.UNAUTHORIZED);
+    });
+
+    test.todo('Abgelaufener Token');
+});
